d99bcc898e0d38434683b03fc3249b594ae0b446
oliver-hull-iii <ohull1@binghamton.edu>
2026-06-27 13:39:49 -0400

    Updates

---
+++ precache-manifest.919235406dd8a14d830656e121ec5b07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0435524601e59d49f9aa69ca1b8844e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "791db5ba9a9f7de25d62",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e7e44e43ba0c2bdf37fa",
+    "url": "/static/css/main.549f30a2.chunk.css"
+  },
+  {
+    "revision": "791db5ba9a9f7de25d62",
+    "url": "/static/js/2.a6716bf1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a6716bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e44e43ba0c2bdf37fa",
+    "url": "/static/js/main.3d499c94.chunk.js"
+  },
+  {
+    "revision": "e8896d9a2a610d9660d8",
+    "url": "/static/js/runtime-main.32b4499a.js"
+  },
+  {
+    "revision": "1ef8bfec03168073466042d1d7f82ce4",
+    "url": "/static/media/alpaca-svgrepo-com.1ef8bfec.svg"
+  }
+]);
